50d006cd070b2679868df5583e6740b1af079f60
Michał Sukiennik <michal.sukiennik@tsc-software.pl>
2022-10-06 11:11:56 +0200

    Powiadomienie o braku użytkowników

--- src/components/User/UserList/UserList.styles.ts
+++ src/components/User/UserList/UserList.styles.ts
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { BORDER_RADIUS } from "../../../Global.styles";
 
 export const ListWrapper = styled.div`
   display: flex;
@@ -20,3 +21,15 @@ export const ListContent = styled.div`
     overflow-x: scroll;
   }
 `;
+
+export const NotFoundWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  background: black;
+  padding: 2rem;
+  color: white;
+  text-align: center;
+  border-radius: ${BORDER_RADIUS}
+`
+
--- src/components/User/UserList/UsersList.tsx
+++ src/components/User/UserList/UsersList.tsx
@@ -3,7 +3,7 @@ import { useUsers } from "../../../hooks/useUsers";
 import { User } from "../../../types/user";
 import PaginationOutlined from "../../Pagination/Pagination";
 import { UserBox } from "../UserBox/UserBox";
-import { ListContent, ListWrapper } from "./UserList.styles";
+import { ListContent, ListWrapper, NotFoundWrapper } from "./UserList.styles";
 import { PaginationWrapper } from "../../Pagination/Pagination.styles";
 import { SortLabel } from "../../Sort/SortLabel";
 import { FilterLabel } from "../../Filter/FilterLabel";
@@ -25,9 +25,13 @@ export const UsersList = () => {
         <EditUser />
         <SortLabel />
         <ListWrapper>
-          {users 
+          {users
             ? (users.map((user, i) => <UserBox user={user} key={i} />)) 
             : <Loader />
+          }
+
+          {
+            users && users.length === 0 && <NotFoundWrapper><h2>Nie znaleziono użytkowników...</h2></NotFoundWrapper> 
           }
         </ListWrapper>
         <PaginationWrapper>
